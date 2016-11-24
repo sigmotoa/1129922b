97fdc3cb1921d44bfaa124f9a0c0c52aa462b6c7
sigmotoa <sigmotoa@misena.edu.co>
2016-11-24 09:32:35 -0500

    Borre cositas

--- src/taberna/Taberna.java
+++ src/taberna/Taberna.java
@@ -14,8 +14,11 @@ public class Taberna {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        
+        
+    
     }
     
 }

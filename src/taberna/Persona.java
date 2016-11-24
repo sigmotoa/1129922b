5591ea684121f0568549e591487a62878b423c35
sigmotoa <sigmotoa@misena.edu.co>
2016-11-24 09:29:41 -0500

    Creacion clase persona

---
+++ src/taberna/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taberna;
+
+/**
+ *
+ * @author SergioIván
+ */
+public class Persona 
+{
+
+    String name;
+    String id;
+    String surname;
+    int birthday[]=new int[3];// 0=dia, 1=mes, 2=año
+    byte age;
+    
+    
+    
+}
